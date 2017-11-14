6f0818bb7e359d0775ed4bae80106d241b176fdc
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-11-15 00:19:01 +0800

    Added test integration using Mockito

--- src/test/java/com/mpe/pc/webmonitoring/services/HddServiceImplTest.java
+++ src/test/java/com/mpe/pc/webmonitoring/services/HddServiceImplTest.java
@@ -4,22 +4,30 @@ import com.mpe.pc.webmonitoring.domains.Hdd;
 import com.mpe.pc.webmonitoring.repositories.HddRepository;
 import com.mpe.pc.webmonitoring.services.HddServiceImpl;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.HashSet;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.*;
 
 public class HddServiceImplTest {
 
-    private final static String MODULE_NO = "HDD123456";
+    private final static String MODULE_NO = "HDD1234567";
 
+    @InjectMocks
     HddServiceImpl hddService;
 
     @Mock
@@ -28,40 +36,61 @@ public class HddServiceImplTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        hddService = new HddServiceImpl(hddRepository);
     }
 
-    @Test
-    public void getAllHddRecords(Pageable pageable) throws Exception {
+//    @Test
+//    public void getAllHddRecords(Pageable pageable) throws Exception {
+//
+//        Hdd hdd = new Hdd();
+//        hdd.setModuleNo(MODULE_NO);
+//        hdd.setFlag(1);
+//
+//        HashSet<Hdd> hddHashSet = new HashSet<>();
+//        hddHashSet.add(hdd);
+//
+//        //hddRepository.save(hdd);
+//        Page<Hdd> hddPage = null;
+//
+//        when(hddService.getAllHddRecords(hdd.getFlag(), pageable)).thenReturn(hddPage);
+//
+//    }
 
-        Hdd hdd = new Hdd();
-        hdd.setModuleNo(MODULE_NO);
-        hdd.setFlag(1);
-
-        HashSet<Hdd> hddHashSet = new HashSet<>();
-        hddHashSet.add(hdd);
-
-        //hddRepository.save(hdd);
-        Page<Hdd> hddPage = null;
-
-        when(hddService.getAllHddRecords(hdd.getFlag(), pageable)).thenReturn(hddPage);
-
-    }
-
-    @Test
-    public void saveHdd() throws Exception {
-    }
+//    @Test
+//    @Ignore
+//    public void saveHdd() throws Exception {
+//    }
 
     @Test
     public void getHdd() throws Exception {
+
+        when(hddRepository.findOne(anyString())).thenReturn(new Hdd());
+
+        Hdd hdd = new Hdd();
+        hdd.setModuleNo(MODULE_NO);
+        hddService.saveHdd(hdd);
+
+        assertNotNull(hddService.getHdd(MODULE_NO));
+
+        verify(hddRepository, times(1)).findOne(anyString());
+        verify(hddRepository, never()).findAll();
     }
 
+//    @Test
+//    @Ignore
+//    public void getHddList() throws Exception {
+//    }
+//
     @Test
-    public void getHddList() throws Exception {
-    }
-
-    @Test
+    @Ignore
     public void deleteHdd() throws Exception {
+        Hdd hdd = new Hdd();
+        hdd.setModuleNo(MODULE_NO);
+
+        hddRepository.save(hdd);
+
+        hddService.deleteHdd(hdd);
+
+        verify(hddRepository, times(1)).delete(any(Hdd.class));
     }
 
 }
