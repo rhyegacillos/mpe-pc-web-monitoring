d1a006e233cb9d7bcf265f3ebe34b08b81d3a327
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-11-16 05:58:21 +0800

    Added test integration using Mockito

---
+++ src/test/java/com/mpe/pc/webmonitoring/controllers/NgPcPartsInventoryControllerTest.java
@@ -0,0 +1,61 @@
+package com.mpe.pc.webmonitoring.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+public class NgPcPartsInventoryControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                    .webAppContextSetup(webApplicationContext)
+                    .apply(SecurityMockMvcConfigurers.springSecurity())
+                    .build();
+
+    }
+
+    @Test
+    public void showForm() throws Exception {
+    }
+
+    @Test
+    public void searchHdd() throws Exception {
+    }
+
+    @Test
+    public void addHdd() throws Exception {
+    }
+
+    @Test
+    public void ngPcPartsTable() throws Exception {
+    }
+
+    @Test
+    public void download() throws Exception {
+    }
+
+    @Test
+    public void updateNgPcParts() throws Exception {
+    }
+
+    @Test
+    public void deleteNgPcParts() throws Exception {
+    }
+
+}
